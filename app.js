f8e7122b62c34a23abae21773113ea1bbf3b33c5
talhahzahid <talhazahid415@gmail.com>
2024-07-30 18:18:41 +0500

    Todo-App

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-// import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
-// import {  getFirestore  } from "./firebaseconfig.js";
+import { collection, addDoc,
+  getDocs,
+}
+ from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js"; 
+ import { db } from "./firebaseconfig.js"
 
 
 
@@ -8,47 +11,95 @@ const input1 = document.querySelector('#input1');
 const ul = document.querySelector('#ul');
 
 const arr = []
-{/* <span class="text-light">${index + 1} =====> </span>
-        <span class="text-light">${item.Todo} <button class="btn btn-danger">Add Todo</button> </span>
-        <br></br> */}
-function renderTodo(){
-  ul.innerHTML = '';
-  arr.map((item,index) => {
-    ul.innerHTML += `
-<div class="container text-center">
-  <div class="row">
-    <div class="col">
-      <span>${index + 1}</span>
-    </div>
-    <div class="col">
-      <span>${item.Todo}</span>
-    </div>
-    <div class="col">
-        <button class="btn btn-success "><i class="fa-solid fa-pen "></i></button> 
-        <button class="btn btn-danger"><i class="fa-solid fa-trash icon"></i></button> 
-        <br>
-        <br>
-    </div>
-  </div>
-</div>
-        `;
+
+async function getData() {
+  const querySnapshot = await getDocs(collection(db, "todos"));
+  querySnapshot.forEach((doc) => {
+    console.log(doc.data());
+    arr.push(doc.data());
   });
+  console.log(arr);
+  renderTodo();
 }
 
-form.addEventListener('submit', (event)=>{
- event.preventDefault();
- arr.push({
-  Todo : input1.value,
- })
- renderTodo();
+getData();
+
+// function renderTodo(){
+//   ul.innerHTML = '';
+//   if (arr.length === 0) {
+//     ul.innerHTML = "no data found";
+//     return;
+//   }
+//   arr.map((item,index) => {
+//     ul.innerHTML += `
+// <div class="container text-center">
+//   <div class="row">
+//     <div class="col">
+//       <span>${index + 1}</span>
+//     </div>
+//     <div class="col">
+//       <span>${item.Todo}</span>
+//     </div>
+//     <div class="col">
+//         <button class="btn btn-success "><i class="fa-solid fa-pen "></i></button> 
+//         <button class="btn btn-danger"><i class="fa-solid fa-trash icon"></i></button> 
+//         <br>
+//         <br>
+//     </div>
+//   </div>
+// </div>
+//         `;
+//   });
+// }
+
+// form.addEventListener('submit',async (event)=>{
+//  event.preventDefault();
+//  arr.push({
+//   Todo : input1.value,
+//  })
+//  renderTodo();
 //  try {
-//     const docRef = await addDoc(collection(db, "todo"), {
-//     todo : Todo.value,
+//     const docRef = await addDoc(collection(db, "users"), {
+//     todo : input1.value,
 //     });
-//     todo.value = ';'
+//     // todo.value = ''
+//     input1.value =''
 //     console.log("Document written with ID: ", docRef.id);
 //   } catch (e) {
 //     console.error("Error adding document: ", e);
 //   }
- input1.value = ''
-})  
+// //  input1.value = ''
+// })  
+
+
+
+
+function renderTodo() {
+  ul.innerHTML = "";
+  if (arr.length === 0) {
+    ul.innerHTML = "no data found";
+    return;
+  }
+  arr.map((item) => {
+    ul.innerHTML += `
+        <li>${item.todo}</li>
+        `;
+  });
+}
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  arr.push({
+    todo: input1.value,
+  });
+  renderTodo();
+  try {
+    const docRef = await addDoc(collection(db, "todos"), {
+      todo: input1.value,
+    });
+    input1.value = "";
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+});
--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,15 +1,15 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-// import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";  
-// const firebaseConfig = {
-//   apiKey: "AIzaSyArz8C9cSgwh-I1z2ZLhZyqAnrv9F7djTY",
-//   authDomain: "user-register-8fa75.firebaseapp.com",
-//   projectId: "user-register-8fa75",
-//   storageBucket: "user-register-8fa75.appspot.com",
-//   messagingSenderId: "1002069799005",
-//   appId: "1:1002069799005:web:6178f0ab0920a5137f9db4",
-//   measurementId: "G-2BVJS2W7PK"
-// };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyArz8C9cSgwh-I1z2ZLhZyqAnrv9F7djTY",
+  authDomain: "user-register-8fa75.firebaseapp.com",
+  projectId: "user-register-8fa75",
+  storageBucket: "user-register-8fa75.appspot.com",
+  messagingSenderId: "1002069799005",
+  appId: "1:1002069799005:web:6178f0ab0920a5137f9db4",
+  measurementId: "G-2BVJS2W7PK"
+};
 
-//  export const app = initializeApp(firebaseConfig);
-// export const db = getFirestore(app)
+ const app = initializeApp(firebaseConfig);
+ export const db = getFirestore(app);
