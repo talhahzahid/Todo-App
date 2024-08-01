f6b56a6032c1c09f13f654292eafbb9e6671b0fe
talhahzahid <talhazahid415@gmail.com>
2024-08-01 15:49:21 +0500

    todo

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-import { collection, addDoc,
+import {
+  collection,
+  addDoc,
   getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
 }
- from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js"; 
- import { db } from "./firebaseconfig.js"
+  from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { db } from "./firebaseconfig.js"
 
 
 
@@ -16,60 +21,13 @@ async function getData() {
   const querySnapshot = await getDocs(collection(db, "todos"));
   querySnapshot.forEach((doc) => {
     console.log(doc.data());
-    arr.push(doc.data());
+    arr.push({ ...doc.data(), id: doc.id });
   });
-  console.log(arr);
   renderTodo();
 }
-
 getData();
 
-// function renderTodo(){
-//   ul.innerHTML = '';
-//   if (arr.length === 0) {
-//     ul.innerHTML = "no data found";
-//     return;
-//   }
-//   arr.map((item,index) => {
-//     ul.innerHTML += `
-// <div class="container text-center">
-//   <div class="row">
-//     <div class="col">
-//       <span>${index + 1}</span>
-//     </div>
-//     <div class="col">
-//       <span>${item.Todo}</span>
-//     </div>
-//     <div class="col">
-//         <button class="btn btn-success "><i class="fa-solid fa-pen "></i></button> 
-//         <button class="btn btn-danger"><i class="fa-solid fa-trash icon"></i></button> 
-//         <br>
-//         <br>
-//     </div>
-//   </div>
-// </div>
-//         `;
-//   });
-// }
 
-// form.addEventListener('submit',async (event)=>{
-//  event.preventDefault();
-//  arr.push({
-//   Todo : input1.value,
-//  })
-//  renderTodo();
-//  try {
-//     const docRef = await addDoc(collection(db, "users"), {
-//     todo : input1.value,
-//     });
-//     // todo.value = ''
-//     input1.value =''
-//     console.log("Document written with ID: ", docRef.id);
-//   } catch (e) {
-//     console.error("Error adding document: ", e);
-//   }
-// //  input1.value = ''
-// })  
 
 
 
@@ -77,29 +35,85 @@ getData();
 function renderTodo() {
   ul.innerHTML = "";
   if (arr.length === 0) {
-    ul.innerHTML = "no data found";
+    ul.innerHTML = "No Data Found";
     return;
   }
-  arr.map((item) => {
+  arr.map((item,index) => {
     ul.innerHTML += `
-        <li>${item.todo}</li>
-        `;
+          <div class="container text-center">
+  <div class="row">
+    <div class="col">
+    ${index + 1}
+    </div>
+    <div class="col">
+      ${item.todo}
+    </div>
+    <div class="col">
+      <button class="deletebtn btn btn-danger">Delete</button>
+       <button class="editbtn btn btn-success">Edit</button>
+    </div>
+  </div>
+</div>
+<br>
+          `;
   });
+  const deletebtn = document.querySelectorAll('.deletebtn');
+  const editbtn = document.querySelectorAll('.editbtn');
+
+  // deletebtn.forEach((btn,index)=>{
+  //   // console.log(btn);
+  // btn.addEventListener('click', async ()=>{
+  //   console.log(arr[index]);
+  //   await deleteDoc(doc(db, "todo", arr[index].id));
+  //   arr.splice(index , 1)
+  //   renderTodo()
+  // })
+  // })
+  deletebtn.forEach((btn, index) => {
+    btn.addEventListener("click", async () => {
+      console.log(arr[index]);
+      await deleteDoc(doc(db, "todos", arr[index].id));
+      console.log("Data deleted");
+      arr.splice(index, 1);
+      renderTodo();
+    });
+  });
+
+
+  editbtn.forEach((btn, index) => {
+    btn.addEventListener("click", async () => {
+      const updatedNewValue = prompt("enter new value");
+      const todoUpdate = doc(db, "todos", arr[index].id);
+      await updateDoc(todoUpdate, {
+        todo: updatedNewValue,
+      });
+      console.log("Data updated");
+      arr[index].todo = updatedNewValue;
+      renderTodo();
+    });
+  });
+
+
 }
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
-  arr.push({
-    todo: input1.value,
-  });
-  renderTodo();
-  try {
+    try {
     const docRef = await addDoc(collection(db, "todos"), {
       todo: input1.value,
     });
-    input1.value = "";
     console.log("Document written with ID: ", docRef.id);
+    arr.push({
+      todo: input1.value,
+      id: docRef.id,
+    })
+    renderTodo()
+    input1.value = "";
   } catch (e) {
     console.error("Error adding document: ", e);
   }
 });
+
+
+
+
